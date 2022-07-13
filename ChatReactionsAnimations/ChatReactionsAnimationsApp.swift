e828ec1b5cdd196fd9e1dfba855787c3b9293f8b
danixuz <danixuz@icloud.com>
2022-07-13 07:35:08 +0300

    Initial Commit

---
+++ ChatReactionsAnimations/ChatReactionsAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatReactionsAnimationsApp.swift
+//  ChatReactionsAnimations
+//
+//  Created by Daniel Spalek on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatReactionsAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
